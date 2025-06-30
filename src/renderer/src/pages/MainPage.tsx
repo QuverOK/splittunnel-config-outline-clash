a106bc115f191299dcfa01f1eb2747c14eeecc23
quverok <quverok.work@gmail.com>
2025-06-30 15:03:35 +0300

    fix(layout): fix centered layout

--- src/renderer/src/pages/MainPage.tsx
+++ src/renderer/src/pages/MainPage.tsx
@@ -3,7 +3,7 @@ import { ConfigForm } from '@renderer/widgets/ConfigForm'
 
 export const MainPage = (): ReactElement => {
   return (
-    <div className="container relative flex min-h-screen flex-col items-center justify-center">
+    <div className="container relative flex min-h-screen min-w-screen flex-col items-center justify-center">
       <div className="w-full max-w-md">
         <h1 className="mb-4 text-center text-2xl font-bold">Clash Config Generator</h1>
         <ConfigForm />
